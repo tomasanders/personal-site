552d3d13c2a1518ffe63a742d1ba764d10eea1aa
Tom Sanders <thomas.a.sanders@gmail.com>
2023-03-25 13:15:00 +0900

    initial state

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,29 +1,5 @@
 import '@/styles/globals.css';
-import { ChakraProvider, extendTheme } from '@chakra-ui/react';
-
-const colors = {
-  ideBg: '#262335',
-  sideBg: '#181520',
-  tabBg: '#241b2f',
-  sideIcon: '#a2a1a5',
-  tabActive: '#840084',
-  tabText: '#eeedef',
-  ideVariable: '#f8db5c',
-  ideFunction: '#df9ad0',
-  ideKeyword: '#44f1ef',
-  ideString: '#1e1b29',
-  ideParam: '#72f1b8',
-  ideComment: '#a9aed1',
-  ideOperator: '#b5b5b6',
-  ideText: '#B0AFB2'
-};
-
-const theme = extendTheme({ colors });
 
 export default function App({ Component, pageProps }) {
-  return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
-  );
+  return <Component {...pageProps} />;
 };
