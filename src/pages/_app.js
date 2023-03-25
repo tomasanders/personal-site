f202566fb90d0c957aecd1232e3414d86b1b8fad
Tom Sanders <thomas.a.sanders@gmail.com>
2023-03-25 11:20:16 +0900

    style: updated palette

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,13 +8,14 @@ const colors = {
   sideIcon: '#a2a1a5',
   tabActive: '#840084',
   tabText: '#eeedef',
-  ideYellow: '#f8db5c',
-  idePink: '#df9ad0',
-  ideTeal: '#44f1ef',
-  ideOrange: '#1e1b29',
-  ideGreen: '#72f1b8',
+  ideVariable: '#f8db5c',
+  ideFunction: '#df9ad0',
+  ideKeyword: '#44f1ef',
+  ideString: '#1e1b29',
+  ideParam: '#72f1b8',
   ideComment: '#a9aed1',
-  ideWhite: '#b5b5b6'
+  ideOperator: '#b5b5b6',
+  ideText: '#B0AFB2'
 };
 
 const theme = extendTheme({ colors });
